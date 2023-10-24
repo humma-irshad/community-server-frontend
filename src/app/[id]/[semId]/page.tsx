0130b52779fb3dbc6bf3bb19645a4f25c6eaa3b9
humma-irshad <hummairshad48@gmail.com>
2023-10-24 20:09:14 +0530

    Add video lecture links

--- src/app/[id]/[semId]/page.tsx
+++ src/app/[id]/[semId]/page.tsx
@@ -1,14 +1,22 @@
 'use client';
 
-import {Box, Button} from '@mui/material';
+import {Box, Button, Typography} from '@mui/material';
 import AttachmentIcon from '@mui/icons-material/Attachment';
+import VideocamOutlined from '@mui/icons-material/VideocamOutlined';
 import {useParams} from 'next/navigation';
 import {useEffect, useState} from 'react';
+import {StyledDivider} from '@/components/home/index.style';
 
 interface IFile {
   name: string;
   url: string;
 }
+
+const videoLectures = [
+  {title: 'NestJS Course for Beginners', url: 'https://youtu.be/W6NZfCO5SIk?si=GSsrLc6AGkZfsUqD'},
+  {title: 'NestJS Course for Beginners', url: 'https://youtu.be/GHTA143_b-s?si=M1fdxY5XFEBYHmIK'},
+  {title: 'ReactJS Course for Beginners', url: 'https://youtu.be/bMknfKXIFA8?si=vCOu6vNRXnUV8eFB'},
+];
 
 export default function SemesterPae() {
   const params = useParams();
@@ -24,16 +32,34 @@ export default function SemesterPae() {
   }, []);
 
   return (
-    <Box sx={{justifyContent: 'center'}}>
+    <Box sx={{marginLeft: '18rem'}}>
+      <Typography align='center' paragraph={true} variant='h4' fontWeight={800}>
+        Notes for You
+      </Typography>
       {files?.map((file) => (
         <Button
-          sx={{marginLeft: '24rem'}}
+          sx={{ml: '3rem'}}
           href={file.url}
           download={true}
           target='blank'
-          endIcon={<AttachmentIcon />}
+          startIcon={<AttachmentIcon />}
         >
           {file.name}
+        </Button>
+      ))}
+      <StyledDivider sx={{margin: '3rem'}} />
+      <Typography align='center' paragraph={true} variant='h4' fontWeight={800}>
+        Videos for You
+      </Typography>
+      {videoLectures?.map((video) => (
+        <Button
+          sx={{ml: '3rem'}}
+          href={video.url}
+          download={true}
+          target='blank'
+          startIcon={<VideocamOutlined />}
+        >
+          {video.title}
         </Button>
       ))}
     </Box>
