0df1cddaca01ac8530f5243e016af6e58873a2b4
humma-irshad <hummairshad48@gmail.com>
2023-10-26 21:36:59 +0530

    Style notes and video links

--- src/app/[id]/[semId]/page.tsx
+++ src/app/[id]/[semId]/page.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import {Box, Button, Typography} from '@mui/material';
-import AttachmentIcon from '@mui/icons-material/Attachment';
+import {Box, Button, Grid, Paper, ThemeProvider, Typography, styled} from '@mui/material';
 import VideocamOutlined from '@mui/icons-material/VideocamOutlined';
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
 import {useParams} from 'next/navigation';
 import {useEffect, useState} from 'react';
-import {StyledDivider} from '@/components/home/index.style';
+import {theme} from '@/theme/theme';
 
 interface IFile {
   name: string;
@@ -13,10 +13,34 @@ interface IFile {
 }
 
 const videoLectures = [
-  {title: 'NestJS Course for Beginners', url: 'https://youtu.be/W6NZfCO5SIk?si=GSsrLc6AGkZfsUqD'},
-  {title: 'NestJS Course for Beginners', url: 'https://youtu.be/GHTA143_b-s?si=M1fdxY5XFEBYHmIK'},
-  {title: 'ReactJS Course for Beginners', url: 'https://youtu.be/bMknfKXIFA8?si=vCOu6vNRXnUV8eFB'},
+  {
+    title: 'JavaScript for Beginners',
+    technology: 'JavaScript',
+    url: 'https://youtu.be/W6NZfCO5SIk?si=GSsrLc6AGkZfsUqD',
+  },
+  {
+    title: 'NestJS for Beginners',
+    technology: 'NestJS',
+    url: 'https://youtu.be/GHTA143_b-s?si=M1fdxY5XFEBYHmIK',
+  },
+  {
+    title: 'ReactJS for Beginners',
+    technology: 'ReactJS',
+    url: 'https://youtu.be/bMknfKXIFA8?si=vCOu6vNRXnUV8eFB',
+  },
 ];
+
+const Item = styled(Paper)(({theme}) => ({
+  ...theme.typography.body1,
+  padding: '1.5rem',
+  textAlign: 'justify',
+  marginLeft: '16.5rem',
+  marginBottom: '1rem',
+  color: theme.palette.text.secondary,
+  height: 'max-content',
+  width: '100%',
+  lineHeight: '60px',
+}));
 
 export default function SemesterPae() {
   const params = useParams();
@@ -32,36 +56,79 @@ export default function SemesterPae() {
   }, []);
 
   return (
-    <Box sx={{marginLeft: '18rem'}}>
-      <Typography align='center' paragraph={true} variant='h4' fontWeight={800}>
+    <>
+      <Typography ml={30} align='center' paragraph={true} variant='h4' fontWeight={800}>
         Notes for You
       </Typography>
-      {files?.map((file) => (
-        <Button
-          sx={{ml: '3rem'}}
-          href={file.url}
-          download={true}
-          target='blank'
-          startIcon={<AttachmentIcon />}
-        >
-          {file.name}
-        </Button>
-      ))}
-      <StyledDivider sx={{margin: '3rem'}} />
-      <Typography align='center' paragraph={true} variant='h4' fontWeight={800}>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <ThemeProvider theme={theme}>
+            <Box
+              sx={{
+                p: 2,
+                borderRadius: 2,
+                bgcolor: 'background',
+                display: 'grid',
+                gridTemplateColumns: {md: '1fr 1fr'},
+                gap: 2,
+              }}
+            >
+              {files?.map((file) => (
+                <Item elevation={6}>
+                  <Typography variant='h4'>{file.name}</Typography>
+                  <Typography variant='h6'>
+                    {params.id.toUpperCase()} - {params.semId}th Semester
+                  </Typography>
+                  <Button
+                    variant='contained'
+                    href={file.url}
+                    download
+                    target='blank'
+                    endIcon={<AutoStoriesIcon />}
+                  >
+                    Read
+                  </Button>
+                </Item>
+              ))}
+            </Box>
+          </ThemeProvider>
+        </Grid>
+      </Grid>
+      <Typography ml={30} align='center' paragraph={true} variant='h4' fontWeight={800}>
         Videos for You
       </Typography>
-      {videoLectures?.map((video) => (
-        <Button
-          sx={{ml: '3rem'}}
-          href={video.url}
-          download={true}
-          target='blank'
-          startIcon={<VideocamOutlined />}
-        >
-          {video.title}
-        </Button>
-      ))}
-    </Box>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <ThemeProvider theme={theme}>
+            <Box
+              sx={{
+                p: 2,
+                borderRadius: 2,
+                bgcolor: 'background',
+                display: 'grid',
+                gridTemplateColumns: {md: '1fr 1fr'},
+                gap: 2,
+              }}
+            >
+              {videoLectures?.map((video) => (
+                <Item elevation={6}>
+                  <Typography variant='h4'>{video.title}</Typography>
+                  <Typography variant='h6'>Technology: {video.technology}</Typography>
+                  <Button
+                    variant='contained'
+                    href={video.url}
+                    download
+                    target='blank'
+                    endIcon={<VideocamOutlined />}
+                  >
+                    Watch
+                  </Button>
+                </Item>
+              ))}
+            </Box>
+          </ThemeProvider>
+        </Grid>
+      </Grid>
+    </>
   );
 }
